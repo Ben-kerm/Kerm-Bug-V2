922e1bc0401feec9c1a38f6f1628db77cc0537ca
Ben-kerm <hanos7275@gmail.com>
2024-10-01 14:36:23 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "237656520674"
+global.ownernomer = "50947567596"
 global.ownername = "Your name"
 global.ytname = "YT: KermHackTools"
 global.socialm = "GitHub: Kgtech-cmr"
 global.location = "Cameroon, douala, yaoundé"
 
-global.ownernumber = '237656520674'  //creator number
+global.ownernumber = '50947567596'  //creator number
 global.ownername = 'your name here' //creatoe name
 global.botname = 'ད𝙆𝙚𝙧𝙢𝘽𝙪𝙜 𝙑2༒' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By'
+global.packname = 'prince alpha'
 global.author = 'ད𝙆𝙚𝙧𝙢𝘽𝙪𝙜 𝙑2༒\n\nYouTube KermHackTools'
 
 //console view/theme
